915ace6d9aaf8d465e92154caef7dfb5d267b1b1
Andls <cnhanw@gmail.com>
2017-02-26 22:07:18 +0800

    Update

--- sync.sh
+++ sync.sh
@@ -5,3 +5,15 @@ base64 list.txt>accnet.txt
 if [ $? -eq 0 ]; then
     echo "Encoded!"
 fi
+
+git commit -a -m "Update"
+
+if [ $? -eq 0 ]; then
+    echo "Updated!"
+fi
+
+git push
+
+if [ $? -eq 0 ]; then
+    echo "Synced!"
+fi
