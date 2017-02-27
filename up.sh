8014738663b20846dc319f4db134bcd47d330953
Andls <cnhanw@gmail.com>
2017-02-27 17:19:21 +0800

    Update

--- up.sh
+++ up.sh
@@ -3,17 +3,29 @@
 base64 -d gfwlist.txt>list.txt
 
 if [ $? -eq 0 ]; then
-    echo -e "\033[32m Decoded! \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m =     Decoded!     = \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
 fi
 
 git commit -a -m "Update"
 
 if [ $? -eq 0 ]; then
-    echo -e "\033[32m Committed! \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m =    Committed!    = \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
 fi
 
 git push
 
 if [ $? -eq 0 ]; then
-    echo -e "\033[32m Pushed! \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m =     Pushed!      = \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
 fi
