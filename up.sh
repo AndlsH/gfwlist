305911303dfcbd2f092804f90e5a0e12177b0a58
Andls <cnhanw@gmail.com>
2017-03-03 09:31:48 +0800

    Update

--- up.sh
+++ up.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+git fetch gfwlist
+
+if [ $? -eq 0 ]; then
+    echo -e "\033[32m ==================== \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m =     Fetched!     = \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
+fi
+
+git merge gfwlist/master
+
+if [ $? -eq 0 ]; then
+    echo -e "\033[32m ==================== \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m =     Merged!      = \033[0m"
+    echo -e "\033[32m =                  = \033[0m"
+    echo -e "\033[32m ==================== \033[0m"
+fi
 base64 -d gfwlist.txt>list.txt
 
 if [ $? -eq 0 ]; then
