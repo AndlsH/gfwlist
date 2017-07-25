ca05467a983f4d6b60cf3b2424ecf2053cb52e6d
Andls <cnhanw@gmail.com>
2017-07-25 18:41:05 +0800

    waiting to fix conflicts

--- sync.sh
+++ sync.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+git merge origin/master
+
+if [ $? -ne 0 ]; then
+    echo -e "\033[31m ====================================== \033[0m"
+    echo -e "\033[31m =                                    = \033[0m"
+    echo -e "\033[31m =          Fix conflicts,            = \033[0m"
+    echo -e "\033[31m =     press any key to continue.     = \033[0m"
+    echo -e "\033[31m =                                    = \033[0m"
+    echo -e "\033[31m ====================================== \033[0m"
+    read -n1
+fi
+
 base64 list.txt>accnet.txt
 
 if [ $? -eq 0 ]; then
