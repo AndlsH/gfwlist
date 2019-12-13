c9ed2d59209a0c6ebc842aa558f817967501d900
EEGod <9635792+eegod@users.noreply.github.com>
2019-12-13 11:18:13 +0800

    Update

--- sync.sh
+++ sync.sh
@@ -12,18 +12,18 @@ if [ $? -ne 0 ]; then
     read -n1
 fi
 
-genpac --proxy="SOCKS5 127.0.0.1:1080" -o gfwlist.pac --user-rule="list.txt"
+# genpac --proxy="SOCKS5 127.0.0.1:1080" -o gfwlist.pac --user-rule="list.txt"
 
-if [ $? -ne 0 ]; then
-    echo -e "\033[31m ==================== \033[0m"
-    echo -e "\033[31m =                  = \033[0m"
-    echo -e "\033[31m =  PAC generated!  = \033[0m"
-    echo -e "\033[31m =                  = \033[0m"
-    echo -e "\033[31m ==================== \033[0m"
-    read -n1
-fi
+# if [ $? -ne 0 ]; then
+#     echo -e "\033[31m ==================== \033[0m"
+#     echo -e "\033[31m =                  = \033[0m"
+#     echo -e "\033[31m =  PAC generated!  = \033[0m"
+#     echo -e "\033[31m =                  = \033[0m"
+#     echo -e "\033[31m ==================== \033[0m"
+#     read -n1
+# fi
 
-base64 list.txt>accnet.txt
+base64 list.txt > accnet.txt
 
 if [ $? -eq 0 ]; then
     echo -e "\033[32m ==================== \033[0m"
@@ -33,16 +33,16 @@ if [ $? -eq 0 ]; then
     echo -e "\033[32m ==================== \033[0m"
 fi
 
-gfwlist2privoxy -p 127.0.0.1:8118 -t http -f gfwlist.action -i accnet.txt
+# gfwlist2privoxy -p 127.0.0.1:8118 -t http -f gfwlist.action -i accnet.txt
 
-if [ $? -eq 0 ]; then
-    echo -e "\033[32m ==================== \033[0m"
-    echo -e "\033[32m =                  = \033[0m"
-    echo -e "\033[32m =   Action file    = \033[0m"
-    echo -e "\033[32m =    generated!    = \033[0m"
-    echo -e "\033[32m =                  = \033[0m"
-    echo -e "\033[32m ==================== \033[0m"
-fi
+# if [ $? -eq 0 ]; then
+#     echo -e "\033[32m ==================== \033[0m"
+#     echo -e "\033[32m =                  = \033[0m"
+#     echo -e "\033[32m =   Action file    = \033[0m"
+#     echo -e "\033[32m =    generated!    = \033[0m"
+#     echo -e "\033[32m =                  = \033[0m"
+#     echo -e "\033[32m ==================== \033[0m"
+# fi
 
 git commit -a -m "Update"
 
